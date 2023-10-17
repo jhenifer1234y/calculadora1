0f2795e6b359bcfcceeae00c0b379d13dd6b0b80
jhenifer1234y <140421686+jhenifer1234y@users.noreply.github.com>
2023-10-17 11:31:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-botao.addEventListener("click", function () {
-  var nomeCliente = nome.value;
-  var quantiaSorvete = quantidade.value;
-  var saborSorvete = sabores.value;
+calcule.addEventListener("click", function () {
+    var a = valorA.value;
+    var b = valorB.value;
+    var op = operacao.value;
+  
+    a = parseInt(a);
+    b = parseInt(b);
+    op = parseInt(op);
+  
+    if(op == 1){
+        alert(a + b)
+    }
 
-  /*alert("nome: " + nomeCliente +  
-  " Qnt: " + quantiaSorvete + 
-  "sabor: " + saborSorvete);*/
 
-  pedidoNome.innerHTML = "Nome: " + nomeCliente;
-  pedidoQuantia.innerHTML = "Quantidade: " + quantiaSorvete;
-  pedidoSabor.innerHTML = "Sabor: " + saborSorvete;
-});
+    else if(op == 2){
+        alert(a - b)
+    }
+
+    if(op == 3){
+        alert(a * b)
+    }
+
+    if(op == 4){
+        alert(a / b)
+    }
+
+    
+ });
+  
